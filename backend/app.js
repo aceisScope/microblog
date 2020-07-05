a73a7718222f6c00a747fa86b332243d05d1787b
Binghui Liu <bertieliu52@gmail.com>
2020-07-05 20:13:25 +0300

    init nodejs backend and react frontend

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+  });
+
+// start the server
+app.listen(8000, () => {
+    console.log('listening on port 8000');
+  });
