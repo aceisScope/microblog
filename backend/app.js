b2b46e58de624bb4856e1d07e56b71ed2e06d46c
Binghui Liu <bertieliu52@gmail.com>
2020-07-06 11:15:56 +0300

    install redux and router

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 
 const app = express();
+app.use(helmet());
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
